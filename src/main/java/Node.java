998b127ce41da074ebd04dba49e6de89ca6db21d
Nova Lader <novalader@gmail.com>
2024-03-15 14:01:37 -0500

    updated AdjList, Nodes, and Edges for basic information

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,14 +1,22 @@
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Node{
     private final int ID;
     private final int floor;
 
-    private List<Edge> edges;
+    private Set<Edge> edges= new HashSet<>();
     public int getID(){return ID;}
     public int getFloor(){return floor;}
+    public Set<Edge> getEdges(){return edges;}
+    public void addEdge(Edge newEdge){
+        edges.add(newEdge);
+    }
     public Node(int c, int f){
         ID =c;
         floor=f;
     }
+
+
 };
