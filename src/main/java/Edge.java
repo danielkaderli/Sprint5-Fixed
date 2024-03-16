f29b177a163c18cf91b7120c56884f4bf547daaa
Nova Lader <novalader@gmail.com>
2024-03-16 18:55:49 -0500

    added getWeight method

--- src/main/java/Edge.java
+++ src/main/java/Edge.java
@@ -1,9 +1,11 @@
 public class Edge {
     private final Node self;
-    private final int weight;
+    private int weight;
 
     public int getWeight(){return weight;}
+    public void setWeight(int w){weight=w;}
     public Node getSelf(){return self;}
+
 
     public Edge(Node node, int w){
         self=node;
