a50d30a9e03073d74bf734c59530bc4aec456671
Nova Lader <novalader@gmail.com>
2024-03-16 23:59:00 -0500

    Moved TransitionWeight to BuildGraph

--- src/main/java/BuildGraph.java
+++ src/main/java/BuildGraph.java
@@ -1,2 +1,29 @@
+import java.util.Iterator;
+
 public class BuildGraph {
+    public void TransitionWeight(Node transition){
+        //placeholder values
+        int floorMin=99;
+        int floorMax=-99;
+        Iterator<Edge> edgeIterator = transition.getEdges().iterator();
+        while(edgeIterator.hasNext()){
+            int floor =edgeIterator.next().getSelf().getFloor();
+            {
+                if (floor<floorMin){
+                    floorMin=floor;
+                }
+                if(floor>floorMax){
+                    floorMax=floor;
+                }
+
+            }
+        }
+        int diff = (floorMax-floorMin);
+        edgeIterator=transition.getEdges().iterator();
+        while(edgeIterator.hasNext()){
+            Edge currEdge= edgeIterator.next();
+            int currWeight=currEdge.getWeight();
+            currEdge.setWeight(currWeight*(diff*3));
+        }
+    }
 }
