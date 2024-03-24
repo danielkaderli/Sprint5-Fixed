62d3a63ad79f23dcfab47e019c74af79e19e1cef
Nova Lader <novalader@gmail.com>
2024-03-24 15:24:30 -0500

    removed unnecessary variable

--- src/main/java/APIMethods.java
+++ src/main/java/APIMethods.java
@@ -5,8 +5,7 @@ public class APIMethods {
     //returns json string of any object type
     public static <T> String gsonify(T record){
         Gson gson = new Gson();
-        String json = gson.toJson(record);
-        return json;
+        return gson.toJson(record);
     }
     //need functions to receive jsons and convert them to usable information for backend
     //need function to pass image files
