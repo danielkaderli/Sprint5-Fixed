bc63d303c5bbe2e9d8554cd7db418d067b83e209
Caleb Smay <calebsmay@my.unt.edu>
2024-04-20 19:32:22 -0500

    Fixed export statements

--- src/client/typescript/session_storage_tools.js
+++ src/client/typescript/session_storage_tools.js
@@ -1,38 +1,38 @@
-export default {};
+"use strict";
+exports.__esModule = true;
+exports.sessionImageRetrieve = exports.sessionImageStore = void 0;
+exports["default"] = {};
 // TOOLS FOR MAKING BRAIN HURT LESS WHEN DEALING WITH IMAGES
 // I don't want to completely redo the way typescript is currently handled, so cannot offload these to another file
 // Would be good for future refactoring though
-export function sessionImageStore(imageIndex: string, imageData: Blob): void{
+function sessionImageStore(imageIndex, imageData) {
     console.log("sessionImageStore() invoked");
-    
     // Convert the data to Base64
-    const reader = new FileReader();
+    var reader = new FileReader();
     reader.readAsDataURL(imageData);
-    reader.onloadend = function() {
-        let base64string = reader.result
-        
+    reader.onloadend = function () {
+        var base64string = reader.result;
         // Do type checking for TypeScript's sanity (apparently the 'string | ArrayBuffer' data type isn't 'string' enough)
-        if(base64string instanceof ArrayBuffer){
-            const decoder = new TextDecoder('utf-8');
+        if (base64string instanceof ArrayBuffer) {
+            var decoder = new TextDecoder('utf-8');
             base64string = decoder.decode(base64string);
-        }else{
-            base64string = (base64string as string);
-            console.log(base64string)
         }
-
+        else {
+            base64string = base64string;
+            console.log(base64string);
+        }
         // Store the Base64 data
         sessionStorage.setItem(imageIndex, base64string);
-    }
+    };
     // sessionStorage.setItem(imageIndex, imageBlob);
 }
-
-export function sessionImageRetrieve(imageIndex: string): string{
+exports.sessionImageStore = sessionImageStore;
+function sessionImageRetrieve(imageIndex) {
     // Returns the image in a url form, can be used as an <img> src to render the image directly from the url
     console.log("sessionImageRetrieve() invoked");
-
     // Retrieve the Base64 url
-    let retrievedData = sessionStorage.retrieveItem(imageIndex);
+    var retrievedData = sessionStorage.retrieveItem(imageIndex);
     // Return that bad boy
     return retrievedData;
-
 }
+exports.sessionImageRetrieve = sessionImageRetrieve;
