aae5d730815021c6da809bbcb9081f20b6190d1a
Kaderli, Daniel <danielkaderli@my.unt.edu>
2024-04-30 01:59:25 -0500

    remove deprecated text_size file

--- src/client/typescript/text_size.js
+++
@@ -1,9 +0,0 @@
-
-// // Execute the following code when the window is fully loaded
-// window.onload = function() {
-
-//     const savedTextSize = localStorage.getItem("textSizePreference");
-//     if (savedTextSize) {
-//         updateTextSize();
-//     }
-// };
